baa9db4080e1863a5cb7dc32d9e58db3a6814028
Felipe Pino <felipepinoredes@gmail.com>
2019-09-12 09:35:25 -0400

    Initial commit

---
+++ precache-manifest.c87e25da06b0a6dcea0ddfe0fa7bf353.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c174e01fb8b08c2a5fb1ffcc22a975c1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "41293f11986ca7981517",
+    "url": "/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "d030eec11b4d0d030017",
+    "url": "/static/js/2.435ca888.chunk.js"
+  },
+  {
+    "revision": "41293f11986ca7981517",
+    "url": "/static/js/main.b91e33cd.chunk.js"
+  },
+  {
+    "revision": "39381191ba0f6df50ba7",
+    "url": "/static/js/runtime~main.300cd688.js"
+  }
+]);
