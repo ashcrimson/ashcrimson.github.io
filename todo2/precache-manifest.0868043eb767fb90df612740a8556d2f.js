812788bf98390a7f264179fa33be971e4482535e
Felipe Pino <felipepinoredes@gmail.com>
2019-09-13 10:28:16 -0400

    mas paginas

---
+++ todo2/precache-manifest.0868043eb767fb90df612740a8556d2f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0398132464009d346cb09f4a7b8b9994",
+    "url": "/index.html"
+  },
+  {
+    "revision": "07f50d5b06e49c5d59d8",
+    "url": "/static/css/main.4b5f0127.chunk.css"
+  },
+  {
+    "revision": "067f29ce93bf6a5ca81e",
+    "url": "/static/js/2.906f4708.chunk.js"
+  },
+  {
+    "revision": "07f50d5b06e49c5d59d8",
+    "url": "/static/js/main.3fed677d.chunk.js"
+  },
+  {
+    "revision": "74e5b205e42929ee8095",
+    "url": "/static/js/runtime~main.ab4abc95.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
