51d6d559f3c5e4652042dd9d023381f5ce1c31c9
Felipe Pino <felipepinoredes@gmail.com>
2019-09-13 13:28:46 -0400

    meme generator

---
+++ memegenerator/precache-manifest.d87626f4d79e2f7e4cd3428ac79f3d26.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cef55f99795790eb1d19467afe91c269",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7f55c14795b03f67143c",
+    "url": "/static/css/main.a47215da.chunk.css"
+  },
+  {
+    "revision": "5dcb2236e9cf4885f70a",
+    "url": "/static/js/2.d7dfeb17.chunk.js"
+  },
+  {
+    "revision": "7f55c14795b03f67143c",
+    "url": "/static/js/main.d2d17386.chunk.js"
+  },
+  {
+    "revision": "ca618aaea166ee350977",
+    "url": "/static/js/runtime~main.e8cee0ab.js"
+  }
+]);
